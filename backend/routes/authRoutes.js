d3a78a2210443d232e907a2fe143d31ac005a411
dhanushjogi <dhanushjogi333.dj@gmail.com>
2026-03-18 13:05:24 +0530

    Create authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {
+  registerUser,
+  login,
+  getUserProfile,
+  updateProfile,
+  changePassword,
+  forgotPassword,
+  resetPassword
+} = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
+
+router.post('/register', registerUser);
+router.post('/login', login);
+router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateProfile);
+router.put('/password', protect, changePassword);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
+
+module.exports = router;
